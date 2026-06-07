b39bc9eb2613f04655a7c1d81549f10bbddee042
Electric714 <150883084+Electric714@users.noreply.github.com>
2026-06-07 12:38:44 -0500

    feat(debug): add file logging to App Group container for rootfs operations

--- app/Roots.m
+++ app/Roots.m
@@ -11,6 +11,31 @@
 #import "NSObject+SaneKVO.h"
 #include "tools/fakefs.h"
 
+// ============================================================
+// DEBUG LOGGING TO APP GROUP CONTAINER
+// Writes to ContainerURL()/ios-linuxkit-debug.log
+// ============================================================
+
+static void DebugLog(NSString *format, ...) {
+    va_list args;
+    va_start(args, format);
+    NSString *message = [[NSString alloc] initWithFormat:format arguments:args];
+    va_end(args);
+
+    NSURL *logURL = [ContainerURL() URLByAppendingPathComponent:@"ios-linuxkit-debug.log"];
+    NSString *timestamped = [NSString stringWithFormat:@"%@ % @\n", [NSDate date], message];
+
+    NSFileHandle *fileHandle = [NSFileHandle fileHandleForWritingAtPath:logURL.path];
+    if (fileHandle) {
+        [fileHandle seekToEndOfFile];
+        [fileHandle writeData:[timestamped dataUsingEncoding:NSUTF8StringEncoding]];
+        [fileHandle closeFile];
+    } else {
+        // Create file if it doesn't exist
+        [timestamped writeToURL:logURL atomically:YES encoding:NSUTF8StringEncoding error:nil];
+    }
+}
+
 static NSURL *RootsDir(void) {
     static NSURL *rootsDir;
     static dispatch_once_t token;
@@ -38,17 +63,22 @@ static NSString *kDefaultRoot = @"Default Root";
 
 - (instancetype)init {
     if (self = [super init]) {
+        DebugLog(@"Roots init started");
         NSError *error = nil;
         NSArray<NSString *> *rootNames = [NSFileManager.defaultManager contentsOfDirectoryAtPath:RootsDir().path error:&error];
+        if (error) {
+            DebugLog(@"ERROR listing roots dir: %@", error);
+        }
         NSAssert(error == nil, @"couldn't list roots: %@", error);
         self.roots = [rootNames mutableCopy];
         if (!self.roots.count) {
-            // import default root
+            DebugLog(@"No roots found - attempting to import default root");
             NSError *error;
             if (![self importRootFromArchive:[NSBundle.mainBundle URLForResource:@"root" withExtension:@"tar.gz"]
                                         name:@"default"
                                        error:&error
                             progressReporter:nil]) {
+                DebugLog(@"CRITICAL: failed to import default root, error %@", error);
                 NSAssert(NO, @"failed to import default root, error %@", error);
             }
             _wantsVersionFile = YES;
@@ -62,6 +92,8 @@ static NSString *kDefaultRoot = @"Default Root";
 
         if ((!self.defaultRoot || ![self.roots containsObject:self.defaultRoot]) && self.roots.count)
             self.defaultRoot = self.roots.firstObject;
+
+        DebugLog(@"Roots init complete. roots=%@ defaultRoot=%@", self.roots, self.defaultRoot);
     }
     return self;
 }
@@ -70,6 +102,7 @@ static NSString *kDefaultRoot = @"Default Root";
     return [NSUserDefaults.standardUserDefaults stringForKey:kDefaultRoot];
 }
 - (void)setDefaultRoot:(NSString *)defaultRoot {
+    DebugLog(@"Setting defaultRoot to: %@", defaultRoot);
     [NSUserDefaults.standardUserDefaults setObject:defaultRoot forKey:kDefaultRoot];
 }
 
@@ -78,6 +111,7 @@ static NSString *kDefaultRoot = @"Default Root";
 }
 
 - (void)syncFileProviderDomains {
+    DebugLog(@"syncFileProviderDomains called (updating=%d)", self.updatingDomains);
     if (self.updatingDomains) {
         self.domainsNeedUpdate = YES;
         return;
@@ -87,8 +121,9 @@ static NSString *kDefaultRoot = @"Default Root";
 
     [NSFileProviderManager getDomainsWithCompletionHandler:^(NSArray<NSFileProviderDomain *> *domains, NSError *error) {
         void (^onError)(NSError *error) = ^(NSError *error) {
-            if (error != nil)
-                NSLog(@"error adjusting domains: %@", error);
+            if (error != nil) {
+                DebugLog(@"FileProvider error: %@", error);
+            }
         };
         onError(error);
         NSMutableOrderedSet<NSString *> *missingRoots = [self.roots mutableCopy];
@@ -96,6 +131,7 @@ static NSString *kDefaultRoot = @"Default Root";
             if ([missingRoots containsObject:domain.identifier]) {
                 [missingRoots removeObject:domain.identifier];
             } else {
+                DebugLog(@"Removing stale FileProvider domain: %@", domain.identifier);
                 [NSFileManager.defaultManager removeItemAtURL:
                  [NSFileProviderManager.defaultManager.documentStorageURL
                   URLByAppendingPathComponent:domain.pathRelativeToDocumentStorage]
@@ -104,6 +140,7 @@ static NSString *kDefaultRoot = @"Default Root";
             }
         }
         for (NSString *rootId in missingRoots) {
+            DebugLog(@"Adding FileProvider domain for root: %@", rootId);
             [NSFileProviderManager addDomain:[[NSFileProviderDomain alloc] initWithIdentifier:rootId
                                                                                   displayName:rootId
                                                                 pathRelativeToDocumentStorage:rootId]
@@ -127,6 +164,7 @@ void root_progress_callback(void *cookie, double progress, const char *message, 
 }
 
 - (BOOL)importRootFromArchive:(NSURL *)archive name:(NSString *)name error:(NSError **)error progressReporter:(id<ProgressReporter> _Nullable)progress {
+    DebugLog(@"importRootFromArchive called for name=%@ archive=%@", name, archive);
     NSAssert(![self.roots containsObject:name], @"root already exists: %@", name);
     struct fakefsify_error fs_err;
     NSURL *destination = [self rootUrl:name];
@@ -144,14 +182,18 @@ void root_progress_callback(void *cookie, double progress, const char *message, 
                                      code:fs_err.code
                                  userInfo:@{NSLocalizedDescriptionKey:
                                                 [NSString stringWithFormat:@"%s, line %d", fs_err.message, fs_err.line]}];
+        DebugLog(@"importRootFromArchive FAILED: %@", *error);
         if (fs_err.type == ERR_CANCELLED)
             *error = nil;
         free(fs_err.message);
         [NSFileManager.defaultManager removeItemAtURL:tempDestination error:nil];
         return NO;
     }
-    if (![NSFileManager.defaultManager moveItemAtURL:tempDestination toURL:destination error:error])
+    DebugLog(@"fakefs_import succeeded for %@", name);
+    if (![NSFileManager.defaultManager moveItemAtURL:tempDestination toURL:destination error:error]) {
+        DebugLog(@"moveItemAtURL failed: %@", *error);
         return NO;
+    }
 
     void (^addRoot)(void) = ^{
         [[self mutableOrderedSetValueForKey:@"roots"] addObject:name];
@@ -160,6 +202,8 @@ void root_progress_callback(void *cookie, double progress, const char *message, 
         dispatch_sync(dispatch_get_main_queue(), addRoot);
     else
         addRoot();
+
+    DebugLog(@"importRootFromArchive SUCCESS for %@", name);
     return YES;
 }
 
@@ -169,30 +213,36 @@ void root_progress_callback(void *cookie, double progress, const char *message, 
     if (!fakefs_export([self rootUrl:name].fileSystemRepresentation,
                        archive.fileSystemRepresentation,
                        &fs_err, (struct progress) {(__bridge void *) progress, root_progress_callback})) {
-        // TODO: dedup with above method
         NSString *domain = NSPOSIXErrorDomain;
         if (fs_err.type == ERR_SQLITE)
             domain = @"SQLite";
         *error = [NSError errorWithDomain:domain
                                      code:fs_err.code
                                  userInfo:@{NSLocalizedDescriptionKey: [NSString stringWithUTF8String:fs_err.message]}];
+        DebugLog(@"exportRootNamed FAILED: %@", *error);
         if (fs_err.type == ERR_CANCELLED)
             *error = nil;
         free(fs_err.message);
         return NO;
     }
+    DebugLog(@"exportRootNamed SUCCESS for %@", name);
     return YES;
 }
 
 - (BOOL)destroyRootNamed:(NSString *)name error:(NSError **)error {
+    DebugLog(@"destroyRootNamed called for %@", name);
     if ([name isEqualToString:self.defaultRoot]) {
         *error = [NSError errorWithDomain:@"ios-linuxkit" code:0 userInfo:@{NSLocalizedDescriptionKey: @"Cannot delete the default filesystem"}];
+        DebugLog(@"destroyRootNamed blocked - cannot delete default root");
         return NO;
     }
     NSAssert([self.roots containsObject:name], @"root does not exist: %@", name);
-    if (![NSFileManager.defaultManager removeItemAtURL:[self rootUrl:name] error:error])
+    if (![NSFileManager.defaultManager removeItemAtURL:[self rootUrl:name] error:error]) {
+        DebugLog(@"destroyRootNamed removeItemAtURL failed: %@", *error);
         return NO;
+    }
     [[self mutableOrderedSetValueForKey:@"roots"] removeObject:name];
+    DebugLog(@"destroyRootNamed SUCCESS for %@", name);
     return YES;
 }
 
@@ -214,11 +264,12 @@ void root_progress_callback(void *cookie, double progress, const char *message, 
         return NO;
     }
     NSAssert([self.roots containsObject:name], @"root does not exist: %@", name);
-    
+
     if (![NSFileManager.defaultManager moveItemAtURL:[self rootUrl:name] toURL:[self rootUrl:newName] error:error])
         return NO;
     NSUInteger index = [self.roots indexOfObject:name];
     [[self mutableOrderedSetValueForKey:@"roots"] replaceObjectAtIndex:index withObject:newName];
+    DebugLog(@"renameRoot %@ -> %@", name, newName);
     return YES;
 }
 
